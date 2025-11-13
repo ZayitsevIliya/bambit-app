0ceb7fe6908b2193dbef43ceabb36cad64d29d4a
Iliya Zayitsev <zayitseviliya@gmail.com>
2025-11-13 14:29:33 +0500

    installed dependencies

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { ref } from 'vue'
+
+const inputFilter = ref()
+const result = ref()
+function search() {
+  result.value = inputFilter.value
+}
+</script>
+
+<template>
+  <header class="flex gap-2 justify-center m-10">
+    <Input class="w-[50%]" v-model="inputFilter"></Input>
+    <Button @click="search">Search</Button>
+  </header>
+</template>
---
+++ src/components/TheTable.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <Table>
+    <TableCaption>A list of your recent invoices.</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]"> Invoice </TableHead>
+        <TableHead>Status</TableHead>
+        <TableHead>Method</TableHead>
+        <TableHead class="text-right"> Amount </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell class="font-medium"> INV001 </TableCell>
+        <TableCell>
+          <Button variant="outline">Button</Button>
+        </TableCell>
+        <TableCell>Credit Card</TableCell>
+        <TableCell class="text-right"> $250.00 </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
